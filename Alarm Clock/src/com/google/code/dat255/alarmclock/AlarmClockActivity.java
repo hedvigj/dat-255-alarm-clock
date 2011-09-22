c1973c122efc82f7e04864834cc0fc2b818b4272
Sickan <Sickan@129.16.177.243>
2011-09-22 14:04:11 +0200

    First Commit

---
+++ Alarm Clock/src/com/google/code/dat255/alarmclock/AlarmClockActivity.java
@@ -0,0 +1,17 @@
+package com.google.code.dat255.alarmclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmClockActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		if (true == Boolean.TRUE) {
+
+		}
+
+	}
+}
