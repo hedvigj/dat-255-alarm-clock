61384518efdd52dd7df8a4bd65981d3b4c8b8d0d
Hedvig <Hedvigjonsson@hotmail.com>
2011-09-29 16:53:13 +0200

    Commit initial classes and interfaces

---
+++ Alarm Clock/src/com/google/code/dat255/alarmclock/GroupActivity.java
@@ -0,0 +1,7 @@
+package com.google.code.dat255.alarmclock;
+
+import android.app.Activity;
+
+public class GroupActivity extends Activity {
+
+}
---
+++ Alarm Clock/src/com/google/code/dat255/alarmclock/IGroupHandler.java
@@ -0,0 +1,5 @@
+package com.google.code.dat255.alarmclock;
+
+public interface IGroupHandler {
+
+}
