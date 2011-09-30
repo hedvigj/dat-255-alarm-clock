7395e41e1f46eccbf3803ab47515c17ac87f9690
Sickan <Sickan@129.16.178.169>
2011-09-30 09:48:43 +0200

    Corrected spelling of logic package

--- Alarm Clock/src/com/google/code/dat255/alarmclock/lodgic/alarmproperties/IAlarmProperty.java
+++
@@ -1,5 +0,0 @@
-package com.google.code.dat255.alarmclock.lodgic.alarmproperties;
-
-public class IAlarmProperty {
-
-}
---
+++ Alarm Clock/src/com/google/code/dat255/alarmclock/logic/alarm/IAlarm.java
@@ -0,0 +1,5 @@
+package com.google.code.dat255.alarmclock.logic.alarm;
+
+public class IAlarm {
+
+}
