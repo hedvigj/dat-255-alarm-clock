824eb45bd1cf396e50e28338f4a07668181294df
Hedvig <Hedvigjonsson@hotmail.com>
2011-09-30 11:26:10 +0200

    Added button test code.

--- Alarm Clock/src/com/google/code/dat255/alarmclock/view/AlarmClockActivity.java
+++ Alarm Clock/src/com/google/code/dat255/alarmclock/view/AlarmClockActivity.java
@@ -2,7 +2,10 @@ package com.google.code.dat255.alarmclock.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.code.dat255.alarmclock.R;
 
@@ -16,16 +19,27 @@ public class AlarmClockActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.homescreen);
 
-		// overviewButton = (Button) findViewById(R.id.overviewButton);
+		overviewButton = (Button) findViewById(R.id.overviewButton);
 
-		// overviewButton.setOnClickListener(new View.OnClickListener() {
-		// @Override
-		// public void onClick(View v) {
-		// Intent intent = new Intent(AlarmClockActivity.this,
-		// OverviewActivity.class);
-		// startActivity(intent);
-		//
-		// }
-		// });
+		overviewButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// Intent i = new Intent(AlarmClockActivity.this,
+				// OverviewActivity.class);
+				// startActivity(i);
+				Toast.makeText(v.getContext(), "hej", Toast.LENGTH_LONG).show();
+			}
+		});
+
+		/*
+		 * overviewButton.setOnClickListener(new View.OnClickListener() {
+		 * 
+		 * @Override public void onClick(View v) { // Intent intent = new
+		 * Intent(AlarmClockActivity.this, // OverviewActivity.class); //
+		 * startActivity(intent);
+		 * 
+		 * Toast.makeText(this, "Hej", Toast.LENGTH_LONG); } });
+		 */
 	}
 }
