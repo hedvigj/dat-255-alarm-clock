14948b96de1fa9373d12456e1e92d40ad8de6c08
Hedvig <Hedvigjonsson@hotmail.com>
2011-09-30 10:50:35 +0200

    Test commit xml files again

--- Alarm Clock/src/com/google/code/dat255/alarmclock/view/AlarmClockActivity.java
+++ Alarm Clock/src/com/google/code/dat255/alarmclock/view/AlarmClockActivity.java
@@ -2,20 +2,30 @@ package com.google.code.dat255.alarmclock.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.code.dat255.alarmclock.R;
 
 public class AlarmClockActivity extends Activity {
+
+	private Button overviewButton;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.homescreen);
 
-		// TEST COMMIT
-		// Test commit Sickan
-		// Second test commit Gustav
-		// Test Hedvig
-		// More test Sickan
+		// overviewButton = (Button) findViewById(R.id.overviewButton);
+
+		// overviewButton.setOnClickListener(new View.OnClickListener() {
+		// @Override
+		// public void onClick(View v) {
+		// Intent intent = new Intent(AlarmClockActivity.this,
+		// OverviewActivity.class);
+		// startActivity(intent);
+		//
+		// }
+		// });
 	}
 }
