a029b52fe21e9199b81cd1b3e9734f94fc0bd218
Hedvig <Hedvigjonsson@hotmail.com>
2011-09-30 10:06:39 +0200

    added xml files for different screens

--- Alarm Clock/src/com/google/code/dat255/alarmclock/view/AlarmClockActivity.java
+++ Alarm Clock/src/com/google/code/dat255/alarmclock/view/AlarmClockActivity.java
@@ -1,17 +1,16 @@
 package com.google.code.dat255.alarmclock.view;
 
-import com.google.code.dat255.alarmclock.R;
-import com.google.code.dat255.alarmclock.R.layout;
-
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.google.code.dat255.alarmclock.R;
 
 public class AlarmClockActivity extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.homescreen);
 
 		// TEST COMMIT
 		// Test commit Sickan
