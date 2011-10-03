9b55bfdada56f32a44fe3667f9bd81b737c09687
Sickan <Sickan@129.16.178.169>
2011-10-03 10:15:40 +0200

    Added a get text method in TextAlarmPrperty

--- Alarm Clock/src/com/google/code/dat255/alarmclock/logic/alarm/properties/TextAlarmProperty.java
+++ Alarm Clock/src/com/google/code/dat255/alarmclock/logic/alarm/properties/TextAlarmProperty.java
@@ -1,12 +1,15 @@
 package com.google.code.dat255.alarmclock.logic.alarm.properties;
 
-
 public class TextAlarmProperty implements IAlarmProperty {
 
 	private final String text;
 
 	public TextAlarmProperty(String text) {
 		this.text = text;
+	}
+
+	public String GetAlarmText() {
+		return this.text;
 	}
 
 	@Override
