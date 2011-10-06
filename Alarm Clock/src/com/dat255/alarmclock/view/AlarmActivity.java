d504ba4002b87881147d6325598db44d4559abaa
AKERSKUUUG <AKERSKUUUG@gmail.com>
2011-10-06 16:06:46 +0200

    Did some UI work for AlarmActivity. It is now possible to check all weekdays to repeat at once. Weekdays to repeat cannot be selected if the repeat option is unchecked.

--- Alarm Clock/src/com/dat255/alarmclock/view/AlarmActivity.java
+++ Alarm Clock/src/com/dat255/alarmclock/view/AlarmActivity.java
@@ -2,7 +2,12 @@ package com.dat255.alarmclock.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.LinearLayout;
 import android.widget.TimePicker;
 import android.widget.TimePicker.OnTimeChangedListener;
 
@@ -18,6 +23,65 @@ public class AlarmActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.alarmscreen);
+
+		CheckBox checkRepeat = (CheckBox) findViewById(R.id.repeatCheckBox);
+		final LinearLayout linearWeekdays = (LinearLayout) findViewById(R.id.weekdays);
+		linearWeekdays.setVisibility(View.GONE);
+
+		// Make the weekdays visible if the checkRepeat checkbox is checked,
+		// invisible otherwise
+		checkRepeat.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					linearWeekdays.setVisibility(View.VISIBLE);
+				} else {
+					linearWeekdays.setVisibility(View.GONE);
+				}
+
+			}
+		});
+
+		final CheckBox checkEveryday = (CheckBox) findViewById(R.id.checkEveryday);
+		final CheckBox[] checks = new CheckBox[7];
+
+		checks[0] = (CheckBox) findViewById(R.id.checkMonday);
+		checks[1] = (CheckBox) findViewById(R.id.checkTuesday);
+		checks[2] = (CheckBox) findViewById(R.id.checkWednesday);
+		checks[3] = (CheckBox) findViewById(R.id.checkThursday);
+		checks[4] = (CheckBox) findViewById(R.id.checkFriday);
+		checks[5] = (CheckBox) findViewById(R.id.checkSaturday);
+		checks[6] = (CheckBox) findViewById(R.id.checkSunday);
+
+		// Sets all weekdays to the same check status as the checkEveryday
+		// CheckBox
+		checkEveryday.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					for (CheckBox c : checks) {
+						c.setChecked(true);
+					}
+				}
+			}
+		});
+
+		// If any one of the weekdays is unchecked, all weekdays are no longer
+		// checked and the checkEveryday checkbox will be unchecked
+		for (CheckBox c : checks) {
+			c.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+				@Override
+				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+					if (!isChecked) {
+						checkEveryday.setChecked(false);
+					}
+				}
+			});
+
+		}
 
 		button = (Button) findViewById(R.id.homeButton);
 		Tools.createOnClickLauncher(button, AlarmActivity.this, HomeActivity.class);
