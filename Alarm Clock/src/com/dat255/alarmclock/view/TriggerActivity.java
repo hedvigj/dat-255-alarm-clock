51dea8ac5e482279e03e2768aaac5b3e6ac26a27
Hedvig <Hedvigjonsson@hotmail.com>
2011-10-07 13:40:55 +0200

    Added buttons to the triggerscreen and added listeners in TriggerActivity

--- Alarm Clock/src/com/dat255/alarmclock/view/TriggerActivity.java
+++ Alarm Clock/src/com/dat255/alarmclock/view/TriggerActivity.java
@@ -5,14 +5,28 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class TriggerActivity extends Activity {
+import com.dat255.alarmclock.R;
+import com.dat255.alarmclock.utilities.Tools;
+
+public class TriggerActivity extends Activity implements OnClickListener {
 	private PowerManager powerManager;
 	private WakeLock wakeLock;
+	private Button snoozeButton;
+	private Button ignoreButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.triggerscreen);
+		snoozeButton = (Button) findViewById(R.id.snoozeButton);
+		ignoreButton = (Button) findViewById(R.id.ignoreButton);
+		ignoreButton.setOnClickListener(this);
+		snoozeButton.setOnClickListener(this);
 
 		// Initialize the WakeLock that will be used to prevent the phone from
 		// sleeping
@@ -30,5 +44,17 @@ public class TriggerActivity extends Activity {
 	public void onPause() {
 		// Allow the phone to sleep
 		wakeLock.release();
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.snoozeButton:
+			Toast.makeText(this, "Alarm will ring again in 5 min", Toast.LENGTH_LONG).show();
+			break;
+		case R.id.ignoreButton:
+			Tools.createOnClickLauncher(ignoreButton, TriggerActivity.this, HomeActivity.class);
+		}
+
 	}
 }
