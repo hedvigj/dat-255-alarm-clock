e650ce6c953539aab4f6a3246a468d9c01f97fac
Hedvig <Hedvigjonsson@hotmail.com>
2011-10-08 13:06:09 +0200

    Added an raw folder in res with test.mp3, corrected the code that didn't work in trigger activity.

--- Alarm Clock/src/com/dat255/alarmclock/utilities/Sound.java
+++ Alarm Clock/src/com/dat255/alarmclock/utilities/Sound.java
@@ -3,21 +3,22 @@ package com.dat255.alarmclock.utilities;
 import android.content.Context;
 import android.media.MediaPlayer;
 
+import com.dat255.alarmclock.R;
+
 public class Sound {
 
-	private static MediaPlayer p = null;
+	private static MediaPlayer player = null;
 
-	public static void soundStart(Context context, int res) {
-		p = MediaPlayer.create(context, res);
-		p.setLooping(true);
-		p.start();
+	public static void soundStart(Context context) {
+		player = MediaPlayer.create(context, R.raw.test);
+		player.start();
 	}
 
 	public static void soundStop(Context context) {
-		if (p != null) {
-			p.stop();
-			p.release();
-			p = null;
+		if (player != null) {
+			player.stop();
+			player.release();
+			player = null;
 		}
 	}
 }
--- Alarm Clock/src/com/dat255/alarmclock/view/TriggerActivity.java
+++ Alarm Clock/src/com/dat255/alarmclock/view/TriggerActivity.java
@@ -1,37 +1,42 @@
 package com.dat255.alarmclock.view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.dat255.alarmclock.R;
 import com.dat255.alarmclock.logic.alarm.AlarmManager;
 import com.dat255.alarmclock.logic.alarm.IAlarm;
+import com.dat255.alarmclock.utilities.Sound;
+import com.dat255.alarmclock.utilities.Tools;
 
-public class TriggerActivity extends Activity {
-	// private PowerManager powerManager;
-	// private WakeLock wakeLock;
-	// private Button snoozeButton;
-	// private Button ignoreButton;
+public class TriggerActivity extends Activity implements OnClickListener {
+	private PowerManager powerManager;
+	private WakeLock wakeLock;
+	private Button snoozeButton;
+	private Button ignoreButton;
+	private IAlarm alarm = null;
+	private long alarmId;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.triggerscreen);
+		snoozeButton = (Button) findViewById(R.id.snoozeButton);
+		snoozeButton.setOnClickListener(this);
+		ignoreButton = (Button) findViewById(R.id.ignoreButton);
+		ignoreButton.setOnClickListener(this);
 
-		/*
-		 * snoozeButton = (Button) findViewById(R.id.snoozeButton); ignoreButton
-		 * = (Button) findViewById(R.id.ignoreButton);
-		 * ignoreButton.setOnClickListener(this);
-		 * snoozeButton.setOnClickListener(this);
-		 */
 		// Initialize the WakeLock that will be used to prevent the phone from
 		// sleeping
-		// powerManager = (PowerManager)
-		// getSystemService(Context.POWER_SERVICE);
-		// wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK
-		// | PowerManager.ON_AFTER_RELEASE, "AlarmClock Wake lock");
+		powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "AlarmClock Wake lock");
 	}
 
 	@Override
@@ -42,31 +47,40 @@ public class TriggerActivity extends Activity {
 	@Override
 	public void onResume() {
 		super.onResume();
+		// Start alarm sound
+		Sound.soundStart(this);
 
 		// Get the id of the alarm that triggered this screen
-		long alarmId = getIntent().getLongExtra("alarmid", 0);
+		alarmId = getIntent().getLongExtra("alarmid", 0);
 
-		IAlarm alarm = AlarmManager.getInstance().findAlarmById(alarmId);
+		alarm = AlarmManager.getInstance().findAlarmById(alarmId);
 
 		Toast.makeText(this, "Alarm triggered: " + alarmId, Toast.LENGTH_LONG).show();
 
 		// Prevent the phone from sleeping
-		// wakeLock.acquire();
+		wakeLock.acquire();
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
 		// Allow the phone to sleep
-		// wakeLock.release();
+		wakeLock.release();
 	}
-	/*
-	 * @Override public void onClick(View view) { switch (view.getId()) { case
-	 * R.id.snoozeButton: Toast.makeText(this, "Alarm will ring again in 5 min",
-	 * Toast.LENGTH_LONG).show(); break; case R.id.ignoreButton:
-	 * Tools.createOnClickLauncher(ignoreButton, TriggerActivity.this,
-	 * HomeActivity.class); }
-	 * 
-	 * }
-	 */
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.snoozeButton:
+			Sound.soundStop(this);
+			Toast.makeText(this, "Alarm will ring again in 5 min", Toast.LENGTH_LONG).show();
+			alarm.disable();
+			break;
+		case R.id.ignoreButton:
+			Sound.soundStop(this);
+
+			Tools.createOnClickLauncher(ignoreButton, TriggerActivity.this, HomeActivity.class);
+		}
+
+	}
 }
